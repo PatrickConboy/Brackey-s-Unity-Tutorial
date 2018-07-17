e791d024497b63b3ac94354877ab7368dced6140
Unknown <rispir45@gmail.com>
2018-07-17 02:34:56 -0400

    Added force to my player object

--- Brackey's Unity Tutorial/Assets/PlayerMovement.cs
+++ Brackey's Unity Tutorial/Assets/PlayerMovement.cs
@@ -1,16 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	// This is a reference to the Rigidbody component called "rb"
+	public Rigidbody rb;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// Update is called once per frame 
+	// FixedUpdate is better when messing with physics stuff
+	void FixedUpdate () {
+		// * Time.deltaTime evens out force refresh for all frame rates
+		rb.AddForce(0, 0, 2000 * Time.deltaTime); 
 	}
 }
